129e6db0e5f50aecf95cd5ff3dac1ccff2888371
Brendan Nguyen <33134005+BrendanNguyenCS@users.noreply.github.com>
2021-11-16 17:06:11 -0500

    Completing project 2

--- project2/src/Sort.java
+++ project2/src/Sort.java
@@ -7,9 +7,35 @@ public class Sort {
     // Entry point.
     public static void main(String[] args) {
         LinkedDeque<String> d = new LinkedDeque<String>();
+        // continue until no more values entered
         while (!StdIn.isEmpty()) {
+            // entered string
             String w = StdIn.readString();
-            // not able to complete
+            // value is less than first string in deque
+            if (!d.isEmpty() && less(w, d.peekFirst())) {
+                d.addFirst(w);
+            // value is greater than last string in deque
+            } else if (!d.isEmpty() && less(d.peekLast(), w)) {
+                d.addLast(w);
+            } else {
+                LinkedStack<String> s = new LinkedStack<String>();      // temp stack
+                // add all values that are less than value to temp stack
+                while(!d.isEmpty() && less(d.peekFirst(), w)) {
+                    String temp = d.removeFirst();
+                    s.push(temp);
+                }
+                // add value to deque
+                d.addFirst(w);
+                // add all values from temp stack back into deque while emptying temp stack
+                while(!s.isEmpty()) {
+                    String temp = s.pop();
+                    d.addFirst(temp);
+                }
+            }
+        }
+        // print out each value of deque on its own line
+        for (String s : d) {
+            StdOut.println(s);
         }
     }
 
